f0f7379ee0baa02e882b7fb867b446e49cbff21b
jgm <abc59684495@gmail.com>
2020-11-18 22:05:02 +0900

    “db

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(morgan(`dav`));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 
-mongoose.connect(`mongodb://4leaf:fourleaf0309@192.168.219.115:27017/admin`, {
+mongoose.connect(`mongodb://4leaf:fourleaf0309@192.168.219.149:27017/admin`, {
     dbName: `EDU_1`,
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -46,6 +46,22 @@ app.get("/lecture", async(req, res) => {
   const result = await Lecture.find({}, {});
 
   res.render("lecture", { dataList: result });
+});
+
+app.get("/board", (req, res) => {
+  res.render("board");
+});
+
+app.get("/test", (req, res) => {
+  res.render("layouts/main");
+});
+
+app.get("/test1", (req, res) => {
+  res.render("sereens/test1");
+});
+
+app.get("/test2", (req, res) => {
+  res.render("sereens/test2");
 });
 
 app.listen(PORT, () => {
